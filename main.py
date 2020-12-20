acc8d3496d26c11260ffe9056d918f83da45c4af
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2020-12-20 13:35:25 +0300

    Create working directory

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys 
+import os
 from PyQt5 import QtWidgets
 
 import design 
@@ -9,8 +10,14 @@ class App(QtWidgets.QMainWindow, design.Ui_MainWindow):
         super().__init__()
         self.setupUi(self)
 
+        # Create working directory
+        if not os.path.isdir(params.WORK_DIR):
+            os.mkdir(params.WORK_DIR)
+
         # Override the title
         self.setWindowTitle(params.WINDOW_TITLE)
+
+
 
 
 if __name__ == '__main__':
--- params.py
+++ params.py
@@ -1,4 +1,8 @@
+from os.path import expanduser
+
 APP_NAME = "ColorTempManager"
 APP_VERSION = "1.0"
 
 WINDOW_TITLE = APP_NAME + " v" + APP_VERSION
+
+WORK_DIR = expanduser("~") + "/.ctm.d"
