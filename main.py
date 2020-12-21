a77c3601051fdf1408587017a0892e073e8b125c
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2020-12-21 21:37:59 +0300

    Simplify the ui.
    Remove night slider (at least in v1.0)

--- main.py
+++ main.py
@@ -26,9 +26,6 @@ class App(QtWidgets.QMainWindow, design.Ui_MainWindow):
         self.dayTempSlider.setMinimum(params.TEMP_MIN_VALUE)
         self.dayTempSlider.setMaximum(params.TEMP_MAX_VALUE)
 
-        self.nightTempSlider.setMinimum(params.TEMP_MIN_VALUE)
-        self.nightTempSlider.setMaximum(params.TEMP_MAX_VALUE)
-
         self.stateCheckBox.stateChanged.connect(self.changeState)
         self.dayTempSlider.valueChanged.connect(self.changeDayValue)
 
@@ -38,12 +35,9 @@ class App(QtWidgets.QMainWindow, design.Ui_MainWindow):
     def updateUI(self):
         # Set values from settings
         self.dayTempSlider.setValue(self.manager.getDayTempValue())
-        self.nightTempSlider.setValue(self.manager.getNightTempValue())
         self.stateCheckBox.setChecked(self.manager.checkIfEnabled())
         
         self.dayTempLabel.setText(str(self.manager.getDayTempValue()))
-        self.nightTempLabel.setText(str(self.manager.getNightTempValue()))
-
 
     def changeState(self, state):
 
