ab249acf605282368f42981e5e02b5ecbdd950c0
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2020-12-24 19:39:10 +0300

    Update server

--- server.py
+++ server.py
@@ -2,14 +2,14 @@ import params
 import json
 import os
 import socket
-import manager 
+
 
 #
 # Receives json objects with commands
 #
 # Possible commands
 #
-# 1) {"seteState": STATE}
+# 1) {"setState": STATE}
 # 2) {"geteState": STATE}
 # 3) {"setDayValue": TEMP}
 # 4) {"getDayValue": TEMP}
@@ -20,6 +20,34 @@ import manager
 # TEMP value must be integer (from 1000 to 25000 according to redshift documentation)
 #
 
+
+
+class Manager:
+    def __init__(self):
+        if not os.path.exists(params.CONFIG_FILE):
+
+            self.config = open(params.CONFIG_FILE, 'w')
+
+            default_settings = {}
+
+            default_settings["day_value"] = params.TEMP_DAY_DEFAULT_VALUE
+            default_settings["night_value"] = params.TEMP_NIGHT_DEFAULT_VALUE
+            default_settings["is_enabled"] = False
+
+            json.dump(default_settings, self.config)
+
+            self.config.close()
+
+        self.config = open(params.CONFIG_FILE, 'r+')
+        self.settings = json.load(self.config)
+
+
+
+    def check_if_enabled(self):
+        return self.settings['is_enabled']
+
+
+
 def main():
 
     server_port = 9090
@@ -27,21 +55,53 @@ def main():
     server_socket.bind(('', server_port))
     server_socket.listen(1)
 
+    manager = Manager()
+
     print( 'The server is ready to receive.')
 
+    # Main loop
+    # Receive and handle commands
     try:
         while True:
             connection_socket, address = server_socket.accept()
-            client_mesg = connection_socket.recv(1024)
-            client_mesg = client_mesg.decode()
+            
+            while True: 
+                client_mesg = connection_socket.recv(1024)
 
-            print("Received message:", client_mesg)
+                print("Received message:", client_mesg)
 
-            server_reply = "".encode()
-    
-            connection_socket.send(server_reply)
+                try:
+                    client_mesg = json.loads(client_mesg.decode())
+                except json.decoder.JSONDecodeError:
+                    break
+                
+
+                response = ""
+
+                print("OK")
+
+                if (client_mesg["command"] == "getValue"):
+                    print("OK")
+                    if (client_mesg["type"] == "state"):
+                        data = {}
+                        data["response"] = manager.settings['is_enabled']
+
+                        response = json.dumps(data).encode()
+
+                    elif (client_mesg["type"] == "temp"):
+                        print("OK")
+                        if (client_mesg["time"] == "day"):
+                            data = {}
+                            data["response"] = manager.settings["day_value"]
+
+                            response = json.dumps(data).encode()
+
+                print("RESP " + response.decode())
+                connection_socket.send(response)
+                
             connection_socket.close()
 
+    # Exit on Ctrl-C
     except KeyboardInterrupt:
         print("\nServer is stopped by Ctrl-C. Exit")
 
